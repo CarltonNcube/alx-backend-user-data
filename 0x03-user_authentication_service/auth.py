ece99ec71a93ef0f1c12f478c370f9400d16ef4a
Carlton Ncube <carltonncube@outlook.com>
2024-04-23 20:57:19 +0200

    Fuction implement the Auth.register_user in the Auth class

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+# This is a comment on the second line
+from auth import Auth
+
+email = 'me@me.com'
+password = 'mySecuredPwd'
+
+auth = Auth()
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
