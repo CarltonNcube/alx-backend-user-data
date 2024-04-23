2724c5d37644fdda24db421066ba0f625d431ca9
Carlton Ncube <carltonncube@outlook.com>
2024-04-23 20:26:46 +0200

    a SQLAlchemy model named User for a database table named users

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,24 +6,15 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class User(Base):
-    """Represents a user in the system."""
 
+class User(Base):
+    """
+    Represents a user in the database.
+    """
     __tablename__ = 'users'
 
-    id: int = Column(Integer, primary_key=True)
-    email: str = Column(String, nullable=False)
-    hashed_password: str = Column(String, nullable=False)
-    session_id: str = Column(String, nullable=True)
-    reset_token: str = Column(String, nullable=True)
-
-    def __init__(self, email: str, hashed_password: str,
-                session_id: str = None, reset_token: str = None):
-        self.email = email
-        self.hashed_password = hashed_password
-        self.session_id = session_id
-        self.reset_token = reset_token
-
-    def __repr__(self):
-        return f"<User(id={self.id}, email={self.email},
-            session_id={self.session_id}, reset_token={self.reset_token})>"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
